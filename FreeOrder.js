c44f70b9e4bedd732581829d49c8d4f872d56f2f
Mouhamed Dhia <48893264+Mouhamed-Dhia@users.noreply.github.com>
2023-05-28 18:08:25 +0100

    Create FreeOrder.js

---
+++ FreeOrder.js
@@ -0,0 +1,11 @@
+/*     
+------This Code By Mouhamed Dhia Fajria--------
+
+ Email    : ----->  " Mouhameddhia@myself.com "
+ whatsApp : ----->  " +21620940425 "
+ 
+ */
+function FreeOrder(email){
+  document.getElementsByName("email")[0].setAttribute("value","email");
+  document.getElementsByClassName("form-group")[2].setAttribute("style","display:none");
+}

f2c89ad04bba64648807dd44e6935a376476907e
Mouhamed Dhia <48893264+Mouhamed-Dhia@users.noreply.github.com>
2023-05-31 21:46:51 +0100

    Create FreeOrder-YouCan.js

---
+++ FreeOrder-YouCan.js
@@ -0,0 +1,16 @@
+/*     
+------This Code By Mouhamed Dhia Fajria--------
+
+ Email    : ----->  " Mouhameddhia@myself.com "
+ whatsApp : ----->  " +21620940425 "
+ 
+ */
+function FreeOrder(email){
+  const elements = document.querySelectorAll('.contact-form');
+  const count = elements.length;
+    if (count=0)
+    {
+      document.getElementsByName("email")[0].setAttribute("value",email);
+      document.getElementsByClassName("form-group")[2].setAttribute("style","display:none");
+    }
+}

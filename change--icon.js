bf9aebd97f83e874f0cd475dbd1367c789a89feb
Mouhamed Dhia <48893264+Mouhamed-Dhia@users.noreply.github.com>
2023-06-29 23:00:49 +0100

    Update and rename change-icon.js to change--icon.js

--- change--icon.js
+++ change--icon.js
@@ -10,5 +10,5 @@ changeIcon("yc-shopping-cart","fa-bag-shopping");
 changeIcon("yc-menu","fa-bars-staggered");
 });
 
-   document.getElementsByClassName("cart-switcher")[0].innerHTML='<div class="currency-value" style="color: rgb(45, 4, 70);"><span class="value">0</span><span class="currency">&nbsp;د.ت</span></div> <div class="cart-icon"><i class="fa-solid fa-bag-shopping" style="color: rgb(45, 4, 70);"></i></div><span class="cart-count" style="display: none;">0</span>';
+   document.getElementsByClassName("cart-switcher")[0].innerHTML='<div class="currency-value" style="color:#ffffff;"><span class="value">0</span><span class="currency">&nbsp;د.ت</span></div> <div class="cart-icon"><i class="fa-solid fa-bag-shopping" style="color:#ffffff;"></i></div><span class="cart-count" style="display: none;">0</span>';
    
